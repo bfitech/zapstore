2af29d21e2cd65e5ecddf061d7aa5e4af6af15dc
Rendra Suroso <rs@cogsci.bandungfe.net>
2016-08-19 03:07:38 +0700

    Show errors on debug mode.

--- common.php
+++ common.php
@@ -1,6 +1,13 @@
 <?php
 
 
+if (DEBUG) {
+	@ini_set('display_errors', 1);
+	@ini_set('display_startup_errors', 1);
+	@error_reporting(E_ALL);
+}
+
+
 /* composer */
 require_once(ZAPLIB . '/vendor/autoload.php');
 
