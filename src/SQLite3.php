94fd981a6dce5347ac564731e4e6a12649e95c1f
irvan <irvan@bfinews.com>
2019-05-06 14:22:38 +0700

    applying type hinting on SQLite3

--- src/SQLite3.php
+++ src/SQLite3.php
@@ -20,7 +20,7 @@ class SQLite3 extends SQL {
 	 *     omitted.
 	 * @param Logger $logger Logger instance.
 	 */
-	public function __construct($params, Logger $logger=null) {
+	public function __construct(array $params, Logger $logger=null) {
 		$params['dbtype'] = 'sqlite3';
 		parent::__construct($params, $logger);
 	}
